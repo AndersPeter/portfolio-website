7c201a2e1ebc28400edfd07b903f33cb7b482819
AndersPeter <anderspeter1982@gmail.com>
2022-11-10 20:09:02 +0100

    Update of website
    - New pages: currentProject and cv
    - New pictures
    - New layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
     extend: {
       colors: {
         brightRed: "hsl(12, 88%, 59%)",
@@ -18,6 +12,12 @@ module.exports = {
         veryDarkBlue: "hsl(233, 12%, 13%)",
         veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
       },
     },
   },
